7ffa44bc0881892d35850e860e10040089be3e8c
tsekhanskaya <anutta2222@gmail.com>
2022-11-14 15:53:45 +0100

    Added image with attached?

--- app/views/recipes/index.json.jbuilder
+++ app/views/recipes/index.json.jbuilder
@@ -4,9 +4,12 @@ json.array! @recipes.each do |recipe|
   json.id recipe.id
   json.title recipe.title
   json.description recipe.description
-  json.image recipe.image
+  json.image recipe.image_url
+  # json.image recipe.image
   json.user_id recipe.user_id
   json.ingredients recipe.ingredients, :id, :title
   json.created_at recipe.created_at
   json.updated_at recipe.updated_at
+
+  json.url recipe_url(recipe, format: :json)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_221_115_121_225) do
+ActiveRecord::Schema[7.0].define(version: 20_221_111_183_727) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,12 +23,39 @@ ActiveRecord::Schema[7.0].define(version: 20_221_115_121_225) do
     t.index ["cart_id"], name: "index_cart_ingredients_on_cart_id"
     t.index ["ingredient_id"], name: "index_cart_ingredients_on_ingredient_id"
   end
+  enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_carts_on_user_id"
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "friendships", force: :cascade do |t|
@@ -52,7 +79,6 @@ ActiveRecord::Schema[7.0].define(version: 20_221_115_121_225) do
   create_table "recipes", force: :cascade do |t|
     t.string "title", null: false
     t.text "description", null: false
-    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
@@ -72,4 +98,6 @@ ActiveRecord::Schema[7.0].define(version: 20_221_115_121_225) do
   add_foreign_key "friendships", "users"
   add_foreign_key "friendships", "users", column: "friend_id"
   add_foreign_key "recipes", "users"
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
