da972f819d57cb343dd892643e0fcc0fc0ca4ede
tsekhanskaya <anutta2222@gmail.com>
2022-11-15 13:56:42 +0100

    Added email without job

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,14 @@
 
 # class UsersController
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show update destroy]
   def index
     @users = User.all
 
     render json: @users
   end
+
+  def show; end
 
   def create
     @user = User.create(users_params)
@@ -30,7 +33,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    if @user.update(users_params)
+      render :show, status: :ok, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def users_params
     params.require(:user).permit(:username, :email, :password)
